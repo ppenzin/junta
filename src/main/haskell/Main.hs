30eaa8507c751ca543a6145ef6e0a88e52c5e13a
Petr Penzin <penzin.dev@gmail.com>
2015-01-28 19:28:23 -0800

    Runnable version, but does not do anything

--- src/main/haskell/Main.hs
+++ src/main/haskell/Main.hs
@@ -1,11 +1,16 @@
 module Main (main) where
 
 import Junta.Juntafile
+import System.Environment
+import System.Process
+import System.Exit
 
 main :: IO ()
 main = readConfig "JuntaOrg.yml"
-       >>= (\config -> print $ config)
-{-
-main = do 
-    print "Welcome to Junta"
--}
+       >>= (\config -> getArgs)
+       >>= parseArgs
+       >>  exit
+
+parseArgs _ = putStrLn "TODO, all broken" >> abort
+exit = exitWith ExitSuccess
+abort = exitWith $ ExitFailure 8
